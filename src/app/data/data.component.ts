d83e2f3baeceede9a875251def686c2977c8006a
thegogoleplexus <thegogolplex@gmail.com>
2017-12-06 18:57:14 +0100

    routing and nav feedback

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    this.feedback();
+  }
+
+  feedback() {
+    let currentUrl = this.router.url; /// this will give you current url
+    let menu_list = document.getElementsByClassName('nav-link');
+    for (let index = 0; index < menu_list.length; index++) {
+      if(currentUrl == menu_list[index].getAttribute("routerLink")) {
+        menu_list[index].className = 'active nav-link nav-text';
+      } else {
+        menu_list[index].className = 'nav-link nav-text';
+      }
+    }
   }
 
 }
