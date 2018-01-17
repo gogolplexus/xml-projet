a01d2338bb59ec13f85f3ea5f54beb0dd5e89696
fatkine <fatou.diop@etu.unice.fr>
2018-01-17 23:10:24 +0100

    mise en place structure de données

---
+++ src/app/data/data.ts
@@ -0,0 +1,298 @@
+export var single: {} = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    },
+    {
+      "name": "Italie",
+      "value": 1200000
+    }
+  ];
+  
+  export var multi: {} = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7300000
+        },
+        {
+          "name": "2011",
+          "value": 8940000
+        }
+      ]
+    },
+  
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "2010",
+          "value": 7870000
+        },
+        {
+          "name": "2011",
+          "value": 8270000
+        }
+      ]
+    },
+  
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "value": 5000002
+        },
+        {
+          "name": "2011",
+          "value": 5800000
+        }
+      ]
+    }
+  ];
+
+  export var listCenter: {} = [
+    {
+      "name": "CRI Bordeaux - Sud-Ouest",
+      "value": 0
+    },
+    {
+      "name": "CRI Grenoble - Rhône-Alpes",
+      "value": 0
+    },
+    {
+      "name": "CRI Lille - Nord Europe",
+      "value": 0
+    },
+    {
+      "name": "CRI Nancy - Grand Est",
+      "value": 0
+    },
+    {
+      "name": "CRI de Paris",
+      "value": 0
+    },
+    {
+      "name": "CRI Sophia Antipolis - Méditerranée",
+      "value": 0
+    }
+  ];
+  
+  export var personneParDomaineDeRecherche = [
+  {
+    "name": "CRI Bordeaux",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+
+  {
+    "name": "CRI Grenoble",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+
+  {
+    "name": "CRI Lille",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+  {
+    "name": "CRI Nancy",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+  {
+    "name": "CRI de Paris",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+  {
+    "name": "CRI Sophia Antipolis",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+  {
+    "name": "CRI Rennes",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  },
+  {
+    "name": "CRI Saclay",
+    "series": [
+      {
+        "name": "Digital Health, Biology and Earth",
+        "value": 0
+      },
+      {
+        "name": "Perception, Cognition and Interaction",
+        "value": 0
+      },
+      {
+        "name": "Applied Mathematics, Computation and Simulation",
+        "value": 0
+      },
+      {
+        "name": "Networks, Systems and Services, Distributed Computing",
+        "value": 0
+      },
+      {
+        "name": "Algorithmics, Programming, Software and Architecture",
+        "value": 0
+      }
+    ]
+  }
+];
